da28d6ffd4e6d21b3a6d431b3193123a1f6b6667
Glenn Wallace <glennw-coding@proton.me>
2026-02-27 17:09:38 +0100

    main.rs update

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use quadrax::backend::{Context, buffer::Buffer};
 fn main() {
     let ctx = Context::new();
     let initial_data = vec![1.0f32, 2.0, 3.0, 4.0];
-    let mut buffer = ctx.create_variable_buffer(&initial_data);
-    println!("Old data: {:?}", buffer.read());
-    buffer.update(&vec![5.0, 2.3, 17.6, 32.0]);
-    println!("New data: {:?}", buffer.read());
+    let mut buffer = ctx.create_coherent_buffer(&initial_data);
+    println!("Old data: {:?}", buffer.read().wait());
+    buffer.write(&vec![5.0, 2.3, 17.6, 32.0]).wait();
+    println!("New data: {:?}", buffer.read().wait());
 }
